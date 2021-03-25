de56b1be02c192324acf954120585dca8ddba99d
aatransh <aatranshpandey@gmail.com>
2021-03-25 10:30:30 +0530

    fb added

---
+++ heading.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCUBMNiPuiZEdf-WApcqkqWJXUhZlE2Opw",
+    authDomain: "event-planner-udaipur.firebaseapp.com",
+    projectId: "event-planner-udaipur",
+    storageBucket: "event-planner-udaipur.appspot.com",
+    messagingSenderId: "234872477923",
+    appId: "1:234872477923:web:8e30678efbbd85b8328dd6",
+    measurementId: "G-Z379Z6T2NQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
